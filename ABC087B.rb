c59e70bfe64920e0acbc515b7b5253795c2be486
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2018-06-13 13:02:13 +0900

    No.4 完了

--- ABC087B.rb
+++ ABC087B.rb
@@ -1,41 +1,18 @@
-coinsList = [500, 100, 50]
-
-coinsNum = Array.new(coinsList.length)
+coinsNum = Array.new(3)
 coinsNum[0] = gets.chomp.to_i # 500円玉
 coinsNum[1] = gets.chomp.to_i # 100円玉
 coinsNum[2] = gets.chomp.to_i # 50円玉
 
 Price = gets.chomp.to_i
 
-coins = [0, 0, 0]
 pattern = 0
 
-if Price % 50 != 0 then
-  print("0\n")
-  exit
-end
-
-coinNumber = 2
-coins[coinNumber] = Price / coinsList[coinNumber]
-
-def check(coins, coinsNum, coinsList)
-  for i in 0...coinsList.length do
-    return false if coins[i] > coinsNum[i]
+for a in 0..coinsNum[0] do
+  for b in 0..coinsNum[1] do
+    for c in 0..coinsNum[2] do
+      pattern += 1 if 500 * a + 100 * b + 50 * c == Price
+    end
   end
-  return true
-end
-
-loop do
-  rate = coinsList[coinNumber - 1] / coinsList[coinNumber]
-  loop do
-    print(coins)
-    coins[coinNumber] -= rate
-    coins[coinNumber - 1] += 1
-    pattern += 1 if check(coins, coinsNum, coinsList)
-    break if coins[coinNumber] < rate
-  end
-  coinNumber -= 1
-  break if coinNumber == 0
 end
 
 p pattern
